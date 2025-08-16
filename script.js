7907dbe467d02a2d4e255345fe286a4a1c2d74b2
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-08-16 18:56:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+// Get the dropdown
+const select = document.getElementById("colorSelect");
+
+// Get the button (first input of type button)
+const button = document.getElementsByTagName("input")[0];
+
+// Add click event to the button
+button.addEventListener("click", () => {
+  // Find the selected option
+  const selectedIndex = select.selectedIndex;
+
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex); // remove that option
+  }
+});
